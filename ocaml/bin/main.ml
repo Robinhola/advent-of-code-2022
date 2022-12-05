9c8545befbd0212864b0e81fce0a9ecc7d4623b5
Robin Elrharbi--Fleury <robin.elrharbifleury@gmail.com>
2022-12-05 13:08:56 +0530

    Day 05! 😰

--- ocaml/bin/main.ml
+++ ocaml/bin/main.ml
@@ -4,13 +4,20 @@ open Advent_2022
 
 let () = print_endline "Welcome to 2022! Let the advent of code... begin!!!"
 
-let solve (module M: Day.T) = 
+let solve (module M: Day.T) =
   print_endline M.name;
   printf "part1:\t%i\n" M.part1;
   printf "part2:\t%i\n" M.part2;
+;;
+
+let solve_string (module M: Day.T_string) =
+  print_endline M.name;
+  printf "part1:\t%s\n" M.part1;
+  printf "part2:\t%s\n" M.part2;
 ;;
 
 let () = solve (module Day01.T)
 let () = solve (module Day02.T)
 let () = solve (module Day03.T)
 let () = solve (module Day04.T)
+let () = solve_string (module Day05.T)
--- ocaml/lib/day.mli
+++ ocaml/lib/day.mli
@@ -6,3 +6,9 @@ module type T = sig
   val part1 : int
   val part2 : int
 end
+
+module type T_string = sig
+  val name : string
+  val part1 : string
+  val part2 : string
+end
