14b73daf2ac45b21638b875e45ff3115a251e410
Robin Elrharbi--Fleury <robin.elrharbifleury@gmail.com>
2022-12-12 01:12:18 +0530

    day11 part2 simplify

--- ocaml/lib/advent_2022/day11.ml
+++ ocaml/lib/advent_2022/day11.ml
@@ -35,8 +35,7 @@ let lines = Advent.read_lines "day11" ~for_tests:
 
 module Monkey = struct
   type t =
-    { name : string
-    ; items : int Queue.t
+    { items : int Queue.t
     ; modulo : int
     ; operation : (int -> int)
     ; test: (int -> int)
@@ -88,17 +87,13 @@ module Monkey = struct
     )
   ;;
 
-  let make ~name ~starting_items ~operation ~test ~if_true ~if_false =
-    { name
-    ; items = read_starting_items starting_items
+  let make ~starting_items ~operation ~test ~if_true ~if_false =
+    { items = read_starting_items starting_items
     ; operation = read_operation operation
     ; test = read_test test ~if_true ~if_false
     ; modulo = read_modulo test
     }
   ;;
-
-  let to_string t = t.name ^ List.to_string (Queue.to_list t.items) ~f:Int.to_string
-
 end
 
 module State = struct
@@ -110,9 +105,9 @@ module State = struct
 
   let rec read t = function
     | [] -> t |> List.rev |> Array.of_list
-    | name :: starting_items :: operation :: test :: if_true :: if_false :: "" :: rest
-    | name :: starting_items :: operation :: test :: if_true :: if_false :: rest ->
-      read (Monkey.make ~name ~starting_items ~operation ~test ~if_true ~if_false :: t) rest
+    | _ :: starting_items :: operation :: test :: if_true :: if_false :: "" :: rest
+    | _ :: starting_items :: operation :: test :: if_true :: if_false :: rest ->
+      read (Monkey.make ~starting_items ~operation ~test ~if_true ~if_false :: t) rest
     | _ -> failwith "impossible"
   ;;
 
@@ -127,22 +122,22 @@ module State = struct
     Queue.enqueue to_.items x
   ;;
 
-  let rec play_turn reduce_worry (t : t) (i : int) (monkey: Monkey.t) =
+  let rec play_turn ~reduce_worry (t : t) (i : int) (monkey: Monkey.t) =
     match monkey.items |> Queue.dequeue with
     | None -> ()
     | Some x ->
-      let x = x |> monkey.operation in
-      let x = x |> reduce_worry in
-      let to_ = x |> monkey.test in
-      throw t ~x ~to_;
+      let x = x
+        |> monkey.operation
+        |> reduce_worry
+      in
+      throw t ~x ~to_:(monkey.test x);
       t.active.(i) <- t.active.(i) + 1;
-      play_turn reduce_worry t i monkey;
+      play_turn ~reduce_worry t i monkey;
   ;;
 
+  let play_turn_part1 = play_turn ~reduce_worry:(fun x -> x / 3)
 
-  let play_turn_part1 = play_turn (fun x -> x / 3)
-
-  let play_turn_part2 modulo = play_turn (fun x -> x % modulo) 
+  let play_turn_part2 modulo = play_turn ~reduce_worry:(fun x -> x % modulo) 
 
   let play_round f t = Array.iteri ~f:(f t) t.array
 
@@ -152,55 +147,7 @@ module State = struct
       play_round f t;
       play_n_rounds f t (n - 1)
   ;;
-
-  let%expect_test "" =
-    let t = make lines in
-    let play_round = play_round play_turn_part1 in
-    play_round t;
-    print_s [%sexp (t.array |> Array.to_list |> List.to_string ~f:Monkey.to_string: string)];
-    play_round t;
-    print_s [%sexp (t.array |> Array.to_list |> List.to_string ~f:Monkey.to_string: string)];
-    play_round t;
-    print_s [%sexp (t.array |> Array.to_list |> List.to_string ~f:Monkey.to_string: string)];
-    play_round t;
-    print_s [%sexp (t.array |> Array.to_list |> List.to_string ~f:Monkey.to_string: string)];
-    play_round t;
-    print_s [%sexp (t.array |> Array.to_list |> List.to_string ~f:Monkey.to_string: string)];
-    play_round t;
-    print_s [%sexp (t.array |> Array.to_list |> List.to_string ~f:Monkey.to_string: string)];
-    [%expect {|
-      "(\"Monkey 0:(20 23 27 26)\"\"Monkey 1:(2080 25 167 207 401 1046)\"\"Monkey 2:()\"\"Monkey 3:()\")"
-      "(\"Monkey 0:(695 10 71 135 350)\"\"Monkey 1:(43 49 58 55 362)\"\"Monkey 2:()\"\"Monkey 3:()\")"
-      "(\"Monkey 0:(16 18 21 20 122)\"\"Monkey 1:(1468 22 150 286 739)\"\"Monkey 2:()\"\"Monkey 3:()\")"
-      "(\"Monkey 0:(491 9 52 97 248 34)\"\"Monkey 1:(39 45 43 258)\"\"Monkey 2:()\"\"Monkey 3:()\")"
-      "(\"Monkey 0:(15 17 16 88 1037)\"\"Monkey 1:(20 110 205 524 72)\"\"Monkey 2:()\"\"Monkey 3:()\")"
-      "(\"Monkey 0:(8 70 176 26 34)\"\"Monkey 1:(481 32 36 186 2190)\"\"Monkey 2:()\"\"Monkey 3:()\")" |}]
-  ;;
-
 end
-
-let%expect_test "" =
-  let t = State.read [] lines in
-  let monkey = Array.get t 0 in
-  let operation_19 = monkey.operation in
-  let divisible_by_23 = monkey.test in
-  print_s [%message (t: Monkey.t array)];
-  print_s [%message (operation_19 1: int) (operation_19 2: int)];
-  print_s [%message (divisible_by_23 15: int) (divisible_by_23 46: int)];
-  [%expect {|
-    (t
-     (((name "Monkey 0:") (items (79 98)) (modulo 23) (operation <fun>)
-       (test <fun>))
-      ((name "Monkey 1:") (items (54 65 75 74)) (modulo 19) (operation <fun>)
-       (test <fun>))
-      ((name "Monkey 2:") (items (79 60 97)) (modulo 13) (operation <fun>)
-       (test <fun>))
-      ((name "Monkey 3:") (items (74)) (modulo 17) (operation <fun>)
-       (test <fun>))))
-    (("operation_19 1" 19) ("operation_19 2" 38))
-    (("divisible_by_23 15" 3) ("divisible_by_23 46" 2)) |}]
-;;
-
 
 let rec multiply_first_two max1 max2 = function
   | [] -> max1 * max2
@@ -242,11 +189,11 @@ let%expect_test "" =
     (t.active (99 97 8 103)) |}]
 ;;
 
-
 module T : sig
   include Day.T
 end = struct
-  let name = "--- Day 11: ---"
+  let name = "--- Day 11: Monkey in the Middle ---"
+
   let part1 =
     let t = State.make lines in
     let t = State.play_n_rounds State.play_turn_part1 t 20 in
